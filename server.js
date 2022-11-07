925ab63ba00469f007fbc5598a290a880da16cc1
Edet Emmanuel Asuquo <59479466+eddy1759@users.noreply.github.com>
2022-11-07 22:16:29 +0100

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const server = require('./app')
-const {PORT} = require('./config/config')
-
-require('./db/dbConfig').connectToDataBase()
-
-
-server.listen(PORT, () => {
-    console.log(`Server is running on ${process.env.NODE_ENV} mode on port ${PORT}`)
-})
